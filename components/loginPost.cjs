53c00b50c2e7fb73e3ac5a39855769fdb4d33cbb
swathisajjan08 <swathi@novaglobal.in>
2025-07-18 13:58:08 +0530

    Update loginPost.cjs

--- components/loginPost.cjs
+++ components/loginPost.cjs
@@ -5,22 +5,20 @@ const config = fs.readFileSync("config.json", "utf-8");
 const configj = JSON.parse(config);
 
 function loginPromise(email) {
-    return new Promise ((resolve,reject)=>{
-         return mysql.createConnection(configj)
-      .then((connection) => {
-        return connection
-          .query("SELECT * FROM users WHERE email = ?", [email])
-          .then(([rows]) => {
-            connection.end();
-            resolve (rows);
-          })
-          .catch((error) => {
-            connection.end();
-            reject(error);
-          });
-      })
-    })
- 
+  return new Promise((resolve, reject) => {
+    return mysql.createConnection(configj).then((connection) => {
+      return connection
+        .query("SELECT * FROM users WHERE email = ?", [email])
+        .then(([rows]) => {
+          connection.end();
+          resolve(rows);
+        })
+        .catch((error) => {
+          connection.end();
+          reject(error);
+        });
+    });
+  });
 }
 
 module.exports = loginPromise;
